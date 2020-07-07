a8f61d8724d63298471f445b416494dbeaa82e3d
Kndrzej <kozinskiandrzej4@gmail.com>
2020-07-07 15:14:06 +0200

    deleted unnecesary files for now

---
+++ Assets/Scripts/LightningManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightningManager : MonoBehaviour
+{
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private LightningPreset Preset;
+    [SerializeField, Range(0, 600)] private float TimeOfDay;
+
+    private void Update()
+    {
+        if(Preset == null)
+        {
+            return;
+        }
+        if (Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime;
+            TimeOfDay %= 600;
+            UpdateLightning(TimeOfDay / 600);
+        }
+        else
+        {
+            UpdateLightning(TimeOfDay / 600);
+        }
+    }
+    private void UpdateLightning(float timePercent)
+    {
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.fogColor.Evaluate(timePercent);
+
+        if(directionalLight != null)
+        {
+            directionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+            directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170, 0));
+        }
+    }
+    private void OnValidate()
+    {
+        if( directionalLight != null)
+        {
+            return;
+        }
+        if(RenderSettings.sun != null)
+        {
+            directionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach(Light light in lights)
+            {
+                if(light.type == LightType.Directional)
+                {
+                    directionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
